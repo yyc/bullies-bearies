ecf962ebcd536d7f28a16e6dc42d0045f34e6e0c
yeojoey <yeojoey@gmail.com>
2015-12-11 19:36:49 +0800

    Add bully scripts

---
+++ Bully.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bully : MonoBehaviour {
+
+	public bool satisfied;
+	public Respawner respawner;
+	public float Speed = 6f;
+	public float Gravity = 20f;
+	public float JumpSpeed = 8f;
+	public float JumpHeight = 20f;	
+	private int facingRight = 1;
+	private Vector3 moveDirection = Vector3.zero;	
+	private CharacterController characterController;
+	public BasketController basketController;
+
+	// Use this for initialization
+	void Start () {
+		characterController =  gameObject.GetComponent<CharacterController>();
+	}
+	
+	void Update () {
+		if (satisfied) {
+			Satisfied();
+		}
+		Move ();
+	}
+
+	void OnMouseDown() {
+
+	}
+
+	void Move() {
+		if (characterController.isGrounded) {
+			moveDirection = new Vector3 (facingRight, 0, 0);
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection *= Speed;
+		} else {
+			moveDirection = new Vector3(facingRight, moveDirection.y, 0);
+			moveDirection = transform.TransformDirection (moveDirection);
+			moveDirection.x *= Speed / 2;
+		}
+		moveDirection.y -= Gravity * Time.deltaTime;
+		characterController.Move (moveDirection * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag("Platform Edge")) {
+			Flip ();
+		}
+		if (other.CompareTag ("Player")) {
+		}
+	}
+	void Satisfied() {
+		//WALK OFF SCREEN
+		respawner.SendMessage ("BullySatisfied", this);
+	}
+
+	void Flip() {
+		facingRight *= -1;
+		Vector3 currScale = transform.localScale;
+		currScale.x *= -1;
+		transform.localScale = currScale;
+	}
+}
---
+++ Respawner.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Respawner : MonoBehaviour
+{
+	public float respawnTime = 10f;	// The amount of time before spawning starts.
+	public GameObject bully;
+	public int width;
+	public int height;
+	private bool dead = false;
+	private float waitInHeaven = 0f;
+	private float nextRespawn = 0f;
+
+	void Start ()
+	{
+	}
+
+	
+	void Update() {
+		if (Time.time >= nextRespawn && dead) { //If the time is due and the enemy is dead
+			Respawn(); //create a new enemy
+			dead = false; //and tell our script it is alive
+		}
+	}
+	
+	private void Respawn ()
+	{
+		float x = Random.Range (-width / 2, width / 2);
+		float y = Random.Range (-height / 2, height / 2);
+		Vector3 place = new Vector3 (x, y, 0);
+		Instantiate(bully, place, Quaternion.identity);
+	}
+
+	void BullySatisfied( Bully bully ) {
+		GameObject.Destroy (bully.gameObject);
+			dead = true;
+			waitInHeaven = Time.time;
+			nextRespawn = Time.time + respawnTime;
+	}
+}
