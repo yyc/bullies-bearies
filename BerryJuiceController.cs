eeac66785279aa074f213f0b177db0cdaf93b2c2
Varun Patro <varun.kumar.patro@gmail.com>
2015-12-11 16:20:06 +0800

    Attempt to transform juice meter

--- BerryJuiceController.cs
+++ BerryJuiceController.cs
@@ -1,30 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System;
 
 public class BerryJuiceController : MonoBehaviour {
-	public int startingJuiceAmount = 0;
+	public const int JUICE_AMOUNT_MAX = 50;
+	public const int JUICE_AMOUNT_INIT = 10;
 	public int juiceAmount = 0;
 	public int goodBerryCount = 0;
 	public int badBerryCount = 0;
 	public int multiplierRate = 0;
 	public GameObject basketObject;
+	public GameObject fullMeter;
+	public GameObject currentMeter;
 
 	private BasketController mainBasketController;
-	// Use this for initialization
-	void Start () {
-		juiceAmount = startingJuiceAmount;
-		mainBasketController = basketObject.GetComponent<BasketController> ();
-	}
 
-	// Update is called once per frame
+	void Start () {
+		juiceAmount = JUICE_AMOUNT_INIT;
+		mainBasketController = basketObject.GetComponent<BasketController> ();
+		InvokeRepeating("UpdateEvery1Sec", 0, 10.0F);
+	}
+		
 	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			this.juiceAmount += 10;
+		}
+
 		this.multiplierRate = mainBasketController.GetTotalMultiplier ();
-		this.juiceAmount += this.multiplierRate;
 		this.goodBerryCount = mainBasketController.GetGoodBerryCount ();
 		this.badBerryCount = mainBasketController.GetBadBerryCount ();
-		print (multiplierRate);
-		print (juiceAmount);
-		print (goodBerryCount);
-		print (badBerryCount);
+	}
+
+	void UpdateEvery1Sec() {
+		int tempAmount = this.juiceAmount + this.multiplierRate;
+		this.juiceAmount = Math.Min (tempAmount, JUICE_AMOUNT_MAX);
+
+		float newScaleX = fullMeter.transform.localScale.x;
+		float newScaleY = (this.juiceAmount / JUICE_AMOUNT_MAX) * fullMeter.transform.localScale.y;
+		currentMeter.transform.localScale = new Vector3 (newScaleX, fullMeter.transform.localScale.y + 1);
+
+		float newX = fullMeter.transform.position.x;
+		float newY = fullMeter.transform.position.y - (newScaleY - fullMeter.transform.localScale.y) / 2;
+		currentMeter.transform.position = new Vector3 (newX, newY);
 	}
 }
